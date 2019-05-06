154e209897162b7818ac82c7bef401ac48b1217f
tusharnikam1 <tushar.nikam@globant.com>
2019-05-06 14:22:53 +0530

    Testing jenkins deployment

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -4,7 +4,7 @@ import App from './app';
 import { Provider } from "react-redux"
 import Store from "./redux/store"
 
-test('renders without crashing', () => {
+test('renders without crashing', () => {  
     const div = document.createElement('div');
     ReactDOM.render(<Provider store={Store}><App/></Provider>, div);
     ReactDOM.unmountComponentAtNode(div);    
